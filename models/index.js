52c8d27bc6e81045172984b9be9d75965fa6cd86
Fatemeh <moufatemeh7@gmail.com>
2023-01-16 16:01:51 +0330

    book,category,useres

--- models/index.js
+++ models/index.js
@@ -3,22 +3,18 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-var moment = require('moment-timezone');
+const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-const x= moment.tz('Asia/Tehran').format('z'); 
-var y = `${x.slice(0,3)}:${x.slice(3)}` // => like +04:30
-
-
 let sequelize;
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "timezone": y
-  });
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
@@ -26,7 +22,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
