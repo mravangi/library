8e7119ede2b24ce02e8d2b66adb7e47111ca576b
Mostafa Ravangi <mravangi92@gmail.com>
2023-01-20 15:07:54 +0330

    action test ..

--- apolloServer/app.js
+++ apolloServer/app.js
@@ -7,7 +7,7 @@ const express = require('express');
 const routes = require('routes');
 const bodyParser = require('body-parser');
 // const graphqlUploadExpress = require('graphql-upload/graphqlUploadExpress.js');
-
+ 
   const app = express(); //4534
 
   app.use(bodyParser.urlencoded({ extended: true }));
