fca6233c50af46465901c2e50a35029a7d53c568
Mostafa Ravangi <mravangi92@gmail.com>
2023-02-08 17:13:09 +0330

    login api development Done!

--- src/schema/user.js
+++ src/schema/user.js
@@ -7,12 +7,20 @@ module.exports = `
         GetMobileNumber(mobile: String!): GetMobileNumberResponde
         GetOtp(code:String!): GetOtpResponde
         GetUserInfo(input: UserInfoInput) : GetUserInfoResponde
+        LoginGetMobileNumber(mobile: String!): LoginGetMobileNumberResponde
+        LoginGetOtp(code: String!): LoginGetOtpResponde
     }
 
     type GetMobileNumberResponde {
         req: Req
         res: Res
         result: GetMobileNumberResult
+    }
+
+    type LoginGetMobileNumberResponde {
+        req: Req
+        res: Res
+        result: LoginGetMobileNumberResult
     }
 
     type GetOtpResponde {
@@ -27,6 +35,12 @@ module.exports = `
         result: GetUserInfoResult
     }
 
+    type LoginGetOtpResponde {
+        req: Req
+        res: Res
+        result: LoginGetOtpResult
+    }
+
     
     input UserInfoInput {
         firstName: String!
@@ -38,6 +52,11 @@ module.exports = `
 
 
     type GetMobileNumberResult{ 
+        ok: Boolean
+        token: String
+    }
+
+    type LoginGetMobileNumberResult{ 
         ok: Boolean
         token: String
     }
@@ -60,5 +79,11 @@ module.exports = `
         gender: Gender!
         mobile: String!
         nationalCode: String!
+    }
+
+    type LoginGetOtpResult {
+        ok: Boolean
+        token: String
+        user: User
     }
 `;
